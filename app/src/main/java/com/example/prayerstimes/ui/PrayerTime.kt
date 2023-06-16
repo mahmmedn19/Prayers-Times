1bf5ecff37a2b654f319c0000488d5f57f86701a
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-16 23:44:03 +0300

    create navigation fragment

---
+++ app/src/main/java/com/example/prayerstimes/ui/PrayerTime.kt
@@ -0,0 +1,28 @@
+package com.example.prayerstimes.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import com.example.prayerstimes.R
+
+class PrayerTime : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_prayer_time, container, false)
+    }
+
+
+}
