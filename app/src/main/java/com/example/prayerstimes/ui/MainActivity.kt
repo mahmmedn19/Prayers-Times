3436606b6f81de631357ef3988fb55648610d815
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-16 23:56:44 +0300

    refactor design

--- app/src/main/java/com/example/prayerstimes/ui/MainActivity.kt
+++ app/src/main/java/com/example/prayerstimes/ui/MainActivity.kt
@@ -3,10 +3,29 @@ package com.example.prayerstimes.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.prayerstimes.R
+import com.example.prayerstimes.databinding.ActivityMainBinding
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment_host)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
