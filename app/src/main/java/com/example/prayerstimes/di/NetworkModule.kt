1707af77186bb7d7992d0da199cca21b84ade1dc
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-18 10:16:36 +0300

    implement NetworkModule

---
+++ app/src/main/java/com/example/prayerstimes/di/NetworkModule.kt
@@ -0,0 +1,67 @@
+/*
+ * *
+ *  * Created by Mohamed Naser on 6 , 2023.
+ *  *
+ *  * Copyright (c) 2023 All rights reserved.
+ *  *
+ *  * Last modified: 6/17/23, 9:51 PM
+ *
+ *
+ */
+
+package com.example.prayerstimes.di
+
+import com.example.prayerstimes.data.remote.network.PrayerService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+    private val BASE_URL = "https://api.aladhan.com/v1/calendar/"
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        client: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory,
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+    @Singleton
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient =
+        OkHttpClient.Builder().apply {
+            addInterceptor(loggingInterceptor)
+        }.build()
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BASIC
+    }
+    @Singleton
+    @Provides
+    fun providePrayerService(retrofit: Retrofit): PrayerService =
+        retrofit.create(PrayerService::class.java)
+
+}
