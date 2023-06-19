57d2c882536f7b28174625f8bbde838b4fb0b71b
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-19 23:27:23 +0300

    handle get location

--- app/src/main/java/com/example/prayerstimes/ui/prayer/PrayerTime.kt
+++ app/src/main/java/com/example/prayerstimes/ui/prayer/PrayerTime.kt
@@ -11,45 +11,110 @@
 
 package com.example.prayerstimes.ui.prayer
 
-import android.widget.Toast
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.prayerstimes.R
 import com.example.prayerstimes.databinding.FragmentPrayerTimeBinding
 import com.example.prayerstimes.ui.base.BaseFragment
-import com.example.prayerstimes.ui.uiState.PrayerTimesUiState
+import com.example.prayerstimes.util.DateUtils.getCurrentMonth
+import com.example.prayerstimes.util.DateUtils.getCurrentYear
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.util.Locale
 
 @AndroidEntryPoint
 class PrayerTime : BaseFragment<FragmentPrayerTimeBinding>() {
     override val TAG: String = this::class.java.simpleName
     override val layoutIdFragment = R.layout.fragment_prayer_time
     override val viewModel: PrayerTimeViewModel by viewModels()
-
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun setup() {
         val adapter = PrayerTimeAdapter(viewModel)
         binding.recyclerPrayer.adapter = adapter
-        viewModel.prayerTimesUiState.observe(viewLifecycleOwner, ::handlePrayerTimesUiState)
+        binding.buttonNext.setOnClickListener {
+            navigateToQiblaFragment()
+        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        getCurrentLocation()
+
     }
 
-    private fun handlePrayerTimesUiState(uiState: PrayerTimesUiState) {
-        val adapter = PrayerTimeAdapter(viewModel)
-        when {
-            uiState.isLoading -> {
-                // Show loading indicator
-            }
-
-            uiState.isError -> {
-                // Show error message
-                Toast.makeText(requireContext(), uiState.message, Toast.LENGTH_SHORT).show()
-            }
-
-            uiState.prayerTimes != null -> {
-                // Update the adapter data with the prayer times
-                adapter.setItems(uiState.prayerTimes)
-            }
+    private fun fetchPrayerTimes(year: Int, month: Int, latitude: Double, longitude: Double) {
+        // Launch the coroutine in the lifecycleScope
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.fetchPrayerTimes(year, month, latitude, longitude)
         }
     }
 
 
+    private fun navigateToQiblaFragment() {
+        val action = PrayerTimeDirections.actionPrayerTimeToQiblah()
+        findNavController().navigate(action)
+    }
+
+
+    @Suppress("DEPRECATION")
+    private fun getLocationName(latitude: Double, longitude: Double): String? {
+        val geocoder = Geocoder(requireActivity(), Locale.getDefault())
+        try {
+            val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+            if (addresses!!.isNotEmpty()) {
+                val address = addresses[0]
+                val sb = StringBuilder()
+                for (i in 0..address.maxAddressLineIndex) {
+                    sb.append(address.getAddressLine(i)).append("\n")
+                }
+                return sb.toString()
+            }
+        } catch (e: IOException) {
+            Log.e(TAG, "Error getting location name: ${e.message}")
+        }
+        return null
+    }
+
+    private fun getCurrentLocation() {
+        if (checkLocationPermission()) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                location?.let {
+                    val locationName = getLocationName(location.latitude, location.longitude)
+                    fetchPrayerTimes(
+                        getCurrentYear(),
+                        getCurrentMonth(),
+                        location.latitude,
+                        location.longitude
+                    )
+                }
+            }
+
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            requireActivity(), ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(), arrayOf(ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION
+        )
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
+        private const val REQUEST_LOCATION_PERMISSION = 1
+    }
 }
--- app/src/main/java/com/example/prayerstimes/ui/prayer/PrayerTimeViewModel.kt
+++ app/src/main/java/com/example/prayerstimes/ui/prayer/PrayerTimeViewModel.kt
@@ -11,8 +11,6 @@
 
 package com.example.prayerstimes.ui.prayer
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.prayerstimes.domain.usecase.GetPrayerTimesUseCase
@@ -20,6 +18,10 @@ import com.example.prayerstimes.ui.uiState.PrayerTimesUiState
 import com.example.prayerstimes.ui.uiState.toPrayerUiStateList
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -27,46 +29,37 @@ import javax.inject.Inject
 class PrayerTimeViewModel @Inject constructor(private val getPrayerTimesUseCase: GetPrayerTimesUseCase) :
     ViewModel(),
     PrayerTimeListener {
-    /*
-        private val _prayerTimesUiState = MutableStateFlow(null)
-        val prayerTimesUiState = _prayerTimesUiState
-    */
+    private val _prayerTimesUiState = MutableStateFlow(PrayerTimesUiState(isLoading = true))
+    val prayerTimesUiState: StateFlow<PrayerTimesUiState> = _prayerTimesUiState
+    private val _navigateToQiblaEvent = MutableSharedFlow<Unit>()
+    val navigateToQiblaEvent: SharedFlow<Unit> = _navigateToQiblaEvent
 
-    private val _prayerTimesUiState = MutableLiveData<PrayerTimesUiState>()
-    val prayerTimesUiState: LiveData<PrayerTimesUiState> get() = _prayerTimesUiState
-
-    init {
+/*    init {
         fetchPrayerTimes()
-    }
+    }*/
 
-    private fun fetchPrayerTimes() {
+    fun fetchPrayerTimes(year: Int, month: Int, latitude: Double, longitude: Double) {
         viewModelScope.launch(Dispatchers.IO) {
-            _prayerTimesUiState.postValue(PrayerTimesUiState(isLoading = true))
+            _prayerTimesUiState.value = PrayerTimesUiState(isLoading = true)
             try {
-                val prayerTimes = getPrayerTimesUseCase(
-                    2023,
-                    6,
-                    30.044565856710083,
-                    31.233066978107168, 5
-                )
+                val prayerTimes = getPrayerTimesUseCase(year, month, latitude, longitude, 5)
                 val prayerUiState = prayerTimes?.toPrayerUiStateList()
-                _prayerTimesUiState.postValue(
+                _prayerTimesUiState.value =
                     PrayerTimesUiState(prayerTimes = prayerUiState, isLoading = false)
-                )
             } catch (e: Exception) {
-                _prayerTimesUiState.postValue(
-                    PrayerTimesUiState(
-                        isError = true,
-                        message = e.message ?: "Error occurred",
-                        isLoading = false
-                    )
+                _prayerTimesUiState.value = PrayerTimesUiState(
+                    isError = true,
+                    message = e.message ?: "Error occurred",
+                    isLoading = false
                 )
             }
         }
     }
 
 
+
     override fun onClickPrayer(prayerTime: Long) {
-        TODO("Not yet implemented")
+
     }
+
 }
