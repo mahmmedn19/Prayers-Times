2308461b58f37451d5659b0b3a3b1bdc66bb2c4c
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-19 19:40:06 +0300

    refactor base fragment

--- app/src/main/java/com/example/prayerstimes/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/prayerstimes/ui/base/BaseFragment.kt
@@ -12,12 +12,14 @@
 package com.example.prayerstimes.ui.base
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.databinding.library.baseAdapters.BR
 import androidx.lifecycle.ViewModel
 
 abstract class BaseFragment<VB : ViewDataBinding> : Fragment() {
