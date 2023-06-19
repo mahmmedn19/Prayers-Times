9017b256af862ac9800a13166c37f993e51cb5bf
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-19 19:44:07 +0300

    implement ui State from entity to ui state

---
+++ app/src/main/java/com/example/prayerstimes/ui/uiState/PrayerUiState.kt
@@ -0,0 +1,33 @@
+package com.example.prayerstimes.ui.uiState
+
+import com.example.prayerstimes.domain.model.TimingsEntity
+
+
+data class PrayerTimesUiState(
+    val isLoading: Boolean = true,
+    val isError: Boolean = false,
+    val message: String = "",
+    val prayerTimes: List<PrayerUiState>? = null
+)
+
+data class PrayerUiState(
+    val fajr: String = "",
+    val sunrise: String = "",
+    val dhuhr: String = "",
+    val asr: String = "",
+    val maghrib: String = "",
+    val isha: String = ""
+)
+
+fun List<TimingsEntity>.toPrayerUiStateList(): List<PrayerUiState> {
+    return map { timingsEntity ->
+        PrayerUiState(
+            fajr = timingsEntity.fajr,
+            sunrise = timingsEntity.sunrise,
+            dhuhr = timingsEntity.dhuhr,
+            asr = timingsEntity.asr,
+            maghrib = timingsEntity.maghrib,
+            isha = timingsEntity.isha
+        )
+    }
+}
